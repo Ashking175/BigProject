1504feb9a138e6094423691e14da4a2d389275e9
batis <mbatista@islstudent.ch>
2025-09-08 19:56:16 +0200

    0.0.2

--- TheGreatOutdoors.py
+++ TheGreatOutdoors.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 def the_great_outdoors():
     BANNER2 = r"""
                      █████████████████████████████████████████████████
@@ -10,10 +12,14 @@ def the_great_outdoors():
                     █  █   █   █   █   █   █   █   █   █   █   █   █  █
                     █  █   █   █   █   █   █   █   █   █   █   █   █  █
                     ███████████████████████████████████████████████████
-
                                        ( )
                                        /|\
                                        / \
 
     """
+    sleep(1)
+    print("")
+    look = input("Look Around: ")
+    sleep(2)
+    print("")
     print(BANNER2)
