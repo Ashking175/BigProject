390d51d1107ca5ee850611563e398fd3c45c8512
Ashking175 <mbatista@islstudent.ch>
2025-09-11 14:50:19 +0200

    0.0.1sda

--- TheGreatOutdoors.py
+++ TheGreatOutdoors.py
@@ -1,6 +1,22 @@
 from time import sleep
+import random
+
+Labubu: r"""        _____
+                 .-'     `-.
+                /  o     o  \
+               |             |
+               |  \       /  |
+                \  '.___.'  /
+                 `-._____.-'
+                   /     \
+                  /       \
+                 |         |
+                  \       /
+                   `-._.-'
+                            """
 
 def the_great_outdoors():
+    outencounters_list = [cowboy, lababubu, ]
     BANNER2 = r"""
                      █████████████████████████████████████████████████
                     █████████████████████ BIG CASINO █████████████████
@@ -27,3 +43,11 @@ def the_great_outdoors():
     # MAKE RANDOM ENCOUNTERS
     print("You see... ")
     sleep(1)
+    print(Labubu)
+
+string_pull = False
+tension = False
+
+while string_pull False:
+    if tension = True:
+        string_pull = True
